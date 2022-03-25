d96367a05a6aa6d987df21cafd9ed47477564f92
Aman Singhal <amansinghal.it1@gmail.com>
2022-03-26 00:18:50 +0530

    Heroku Deployment settings.

--- Assignment/settings.py
+++ Assignment/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+# Activate Django-Heroku.
+import django_heroku
 
+django_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-766b3ibs2mjd+l*ma(ctog0ptro3!8&fb0!4%#0ybb&cmq5^+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = 'account.MyUser'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['squareboatassignment.herokuapp.com']
 
 
 # Application definition
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
